6de2793c6d585e6741b472d9902cd7e932e42d44
Sergei Schirokow <sschirokow@freenet.de>
2024-04-20 12:54:07 +0200

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1. Versuch</title>
+</head>
+<body>
+    <h1>Meine erste Seite</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas provident ducimus ratione perspiciatis quidem, atque quas dolor. Sapiente aut eum dolor totam ea hic itaque, a excepturi laboriosam voluptatum vero voluptas recusandae quibusdam et doloremque, aspernatur facilis. Harum ipsum eius illo incidunt nesciunt eum dolore dignissimos tempora, numquam explicabo fugiat fuga nobis! Ad natus architecto modi rerum tempore, eos repellat sit in nulla distinctio pariatur veniam ipsa! Blanditiis dolorum nihil, ducimus corrupti explicabo nam ut. Nesciunt corrupti neque voluptatum quia incidunt quibusdam blanditiis rerum temporibus amet distinctio. Molestiae veniam, nemo, aut dolor dolorem ipsam voluptatibus blanditiis quibusdam repellat et placeat excepturi, sint maxime quas. Voluptatibus, asperiores. Tempore possimus in error perspiciatis quia ab, similique repudiandae iste cum laborum, beatae iure suscipit aperiam dolorem aliquam quaerat ipsum! Quas adipisci dicta unde nisi, in harum labore a qui eius maiores deleniti, obcaecati, ad fugit hic repudiandae earum. Iste beatae optio ducimus fugit saepe officia consequuntur natus, neque cum magnam voluptates odit unde sed itaque impedit nam possimus ipsum doloribus quae consectetur nulla odio vero libero quis! Saepe, ut praesentium! Molestiae saepe reiciendis expedita vel, cupiditate illo. Inventore cupiditate iste placeat. Quod vel, libero aliquid velit eos possimus cum eligendi quo quibusdam doloribus.
+    </p>
+    <p>
+    <?php  
+    print("Hallo World");
+
+    echo"So geht es auch... ist aber veraltet";
+    
+    phpinfo();
+
+    ?>
+    </p>
+    
+</body>
+</html>
